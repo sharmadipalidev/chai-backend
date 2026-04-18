9a2f1188e8fe4c4d871c98aefc21e11c8bdfad45
Dipali Sharma <sharmadipali2000@gmail.com>
2026-04-18 15:48:54 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -42,24 +42,4 @@ import {DB_NAME} from "./constants.js";
 
 
 
-/*
-import express from "express";
-const app = express()(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    app.on("error", (error) => {
-      console.log("ERROR: ", error);
-      throw error;
-    });
 
-    app.listen(process.env.PORT, () => {
-      console.log(`App is listening on port ${process.env.PORT}`);
-    });
-  } catch (error) {
-    console.error("ERROR: ", error);
-    throw err;
-  }
-})();
-
-
-*/
