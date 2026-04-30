33c42a4c8d109dbf40a871080b3d04061a9876e6
Dipali Sharma <sharmadipali2000@gmail.com>
2026-04-30 18:10:08 +0530

    Add getUserChannelProfile endpoint to fetch user channel details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -292,7 +292,7 @@ const updateUserAvatar = asyncHandler(async (req, res) => {
   }
 
   //todo: delete old image - assignment
-  //const oldAvatarDelete = 
+  //const oldAvatarDelete =
   const avatar = await uploadOnCloudinary(avatarLocalPath);
 
   if (!avatar.url) {
@@ -340,6 +340,77 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
   return res
     .status(200)
     .json(new ApiResponce(200, user, "Cover Image file updated successfully"));
+});
+
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+  const { username } = req.params;
+
+  if (!username?.trim()) {
+    throw new ApiError(400, "Username is missing");
+  }
+
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username?.toLowerCase(),
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers",
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo",
+      },
+    },
+    {
+      $addFields: {
+        subscribersCount: {
+          $size: "$subscribers",
+        },
+        channelsSubscribedCount: {
+          $size: "$subscribedTo",
+        },
+        isSubscribed: {
+          $cond: {
+            if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        fullname: 1,
+        username: 1,
+        subscribersCount: 1,
+        channelsSubscribedCount: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1,
+      },
+    },
+  ]);
+
+  if (!channel?.length) {
+    throw new ApiError(404, "chennel does not exixts");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponce(200, channel[0], "User channel fetched successfully")
+    );
 });
 
 export {
